b9fbd5953693b6e07727932a85730389d4523dcd
Deivis Fernández <espontaneoclips@gmail.com>
2023-11-09 23:40:18 -0400

    config

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT ?? 4000;
 
-app.use(express.static("public"))
+app.use(express.static("client/dist"))
 
 
 server.listen(PORT, ()=>{
