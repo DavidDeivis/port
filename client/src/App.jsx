021ac77f1cc134a5cfa1c4cb6543a9439432129b
Deivis Fernández <espontaneoclips@gmail.com>
2023-11-13 10:48:25 -0400

    five

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import './App.css'
 import Leaf from './components/Leaf.jsx';
+import io from "socket.io-client";
+
+const socket = io();
 
 function App() {
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 import http from "node:http";
 import express from "express";
+import {Server as socketServer} from "socket.io";
 
 
 const app = express();
 const server = http.createServer(app);
+const io = new socketServer(server);
 
 app.use(express.static("client/dist"));
 
@@ -11,6 +13,12 @@ app.get("/", (req, res)=>{
 
     console.log("a user connected")
     
+})
+
+io.on("connection", (socket)=>{
+
+    console.log("a socket connected")
+
 })
 
 server.listen(process.env.PORT ?? 4000, ()=>{
